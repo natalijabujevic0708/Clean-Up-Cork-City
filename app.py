8a4ad8ebe0e72d70ddd19a1648eb43d6816851c6
Natalija Bujevic <natalijabujevic0708@gmail.com>
2020-08-10 23:26:51 +0100

    Added a form and a card display at locations.html, added the insert location function at app.py

--- app.py
+++ app.py
@@ -1,12 +1,34 @@
 import os
 from flask import Flask
 
+
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello():
-    return 'Hello World ...again'
+def index():
+    return render_template("index.html")
+
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+
+@app.route('/contact')
+def contact():
+    return render_template("contact.html")
+    
+@app.route('/locations')
+def locations():
+    return render_template("locations.html", locations=mongo.db.locations.find())
+
+@app.route('/insert_location', methods=['POST'])
+def insert_location():
+    locations = mongo.db.locations
+    locations.insert_one(request.form.to_dict())
+    return redirect(url_for('locations'))
+
 
 
 if __name__ == '__main__':
