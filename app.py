08ca83c041cc9663239f65e51b293ce63ca4fd74
Natalija Bujevic <natalijabujevic0708@gmail.com>
2020-09-02 10:41:00 +0100

    Added the key status for every object indicating is it still active or deleted, created a new route for deleting locations, added comments to explain the code

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
-import os, time
+import os
+import time
 from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
-from bson.objectid import ObjectId 
-from  geopy.geocoders import Nominatim
+from bson.objectid import ObjectId
+from geopy.geocoders import Nominatim
 
 app = Flask(__name__)
+mongo = PyMongo(app)
+
 
 @app.route('/')
 def index():
@@ -19,63 +22,102 @@ def about():
 @app.route('/contact')
 def contact():
     return render_template("contact.html")
-    
+
+
 @app.route('/locations')
 def locations():
-    return render_template("locations.html", locations=mongo.db.locations.find())
+    return render_template("locations.html", locations=mongo.db.locations.find({"status":"active"}))
 
 
-@app.route('/insert_location', methods=['POST'])
+@app.route('/insert_location', methods=['POST'])  # form on locations.html
 def insert_location():
     if 'picture' in request.files:
-        picture=request.files['picture']
+        picture = request.files['picture']
         mongo.save_file(picture.filename, picture)
+        # using geolocator to determine latitude and longitude
         geolocator = Nominatim(user_agent="natalijabujevic0708@gmail.com")
-        city = "Cork"
-        country = "Ireland"
         address = request.form.get('address')
-        loc = geolocator.geocode(address + ','+ city +','+ country)
-        mongo.db.locations.insert({'address': request.form.get('address'), 'picture_name': picture.filename, 
-        'name': request.form.get('name'),'email': request.form.get('email'), 
-        'date' : time.strftime("%Y-%m-%d %H:%M:%S"),"latitude": loc.latitude, "longitude" : loc.longitude})
+        loc = geolocator.geocode(address)
+        # inserting data in the MongoDB
+        mongo.db.locations.insert({
+            'status' : 'active',
+            'address': address,
+            'picture_name': picture.filename,
+            'uploaded_by': request.form.get('name'),
+            'email': request.form.get('email'),
+            'date': time.strftime("%Y-%m-%d %H:%M:%S"),
+            'latitude_of_location': loc.latitude,
+            'longitude_of_location': loc.longitude
+            })
         return redirect(url_for('locations'))
 
-@app.route('/edit_location/<location_id>')
-def edit_location(location_id):
-    the_location =  mongo.db.locations.find_one({"_id": ObjectId(location_id)})
-    return render_template('editlocation.html', location = the_location)
-
-@app.route('/update_location/<location_id>', methods=["POST"])
-def update_location(location_id):
-    locations = mongo.db.locations
-    locations.update( {'_id': ObjectId(location_id)},
-    {
-        'name':request.form.get('name'),
-        'address':request.form.get('address'),
-        'email': request.form.get('email'),
-        'picture': request.form.get('picture'),
-    })
-    return redirect(url_for('locations'))
-
-@app.route('/<picture_name>') 
-def picture (picture_name): 
+# options under every location - more details, edit or delete location
+@app.route('/<picture_name>')  # to render the picture from MongoDB
+def picture(picture_name):
     return mongo.send_file(picture_name)
 
 @app.route('/location_details/<location_id>')
 def location_details(location_id):
-    the_location_details = mongo.db.locations.find_one({"_id": ObjectId(location_id)})
+    the_location_details = mongo.db.locations.find_one(
+        {"_id": ObjectId(location_id)})
     address = the_location_details['address']
-    name = the_location_details['name']
+    name = the_location_details['uploaded_by']
     date = the_location_details['date']
-    src =url_for('picture', picture_name = the_location_details['picture_name'])
-    return render_template('location_details.html', src = src, address = address, name = name, date = date)
+    src = url_for('picture', picture_name=the_location_details['picture_name'])
+    return render_template('location_details.html', src=src, address=address, name=name, date=date)
+
+
+@app.route('/edit_location/<location_id>')
+def edit_location(location_id):
+    the_location = mongo.db.locations.find_one({"_id": ObjectId(location_id)})
+    return render_template('editlocation.html', location=the_location)
+
+
+@app.route('/update_location/<location_id>', methods=["POST"])
+def update_location(location_id):
+    locations = mongo.db.locations
+    # using geolocator to determine latitude and longitude
+    geolocator = Nominatim(user_agent="natalijabujevic0708@gmail.com")
+    address = request.form.get('address')
+    loc = geolocator.geocode(address)
+    locations.update({'_id': ObjectId(location_id)},
+    {   'status' : 'active',
+        'address_of_location': address,
+        'picture_name': request.form.get('picture'),
+        'uploaded_by': request.form.get('name'),
+        'email': request.form.get('email'),
+        'date': time.strftime("%Y-%m-%d %H:%M:%S"),
+        'latitude_of_location': loc.latitude,
+        'longitude_of_location': loc.longitude
+        })
+    return redirect(url_for('locations'))
 
 @app.route('/delete_location/<location_id>')
 def delete_location(location_id):
-    mongo.db.locations.remove({'_id': ObjectId(location_id)})
+    the_location = mongo.db.locations.find_one({"_id": ObjectId(location_id)})
+    return render_template('deletelocation.html', location=the_location)
+
+@app.route('/delete_location_update/<location_id>', methods=["POST"])
+def delete_location_update(location_id):
+    locations = mongo.db.locations
+    # using geolocator to determine latitude and longitude
+    geolocator = Nominatim(user_agent="natalijabujevic0708@gmail.com")
+    address = request.form.get('address')
+    loc = geolocator.geocode(address)
+    locations.update({'_id': ObjectId(location_id)},
+    {   'status' : 'deleted',
+        'address': address,
+        'reason_for_deleting': request.form.get('reason'),
+        'deleted_by': request.form.get('name'),
+        'email': request.form.get('email'),
+        'date': time.strftime("%Y-%m-%d %H:%M:%S"),
+        'latitude_of_location': loc.latitude,
+        'longitude_of_location': loc.longitude
+        })
     return redirect(url_for('locations'))
 
+
 if __name__ == '__main__':
-    app.run(host=os.environ.get('IP'),
-            port=int(os.environ.get('PORT', '5000')),
-            debug=True)
+    app.run(host = os.environ.get('IP'),
+            port = int(os.environ.get('PORT', '5000')),
+            debug = True)
