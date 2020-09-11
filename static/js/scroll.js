a795b05edd21857686ad3e8c783cd03b04ccd495
Natalija Bujevic <natalijabujevic0708@gmail.com>
2020-09-11 10:26:56 +0100

    Deleted the scroll animation that was unnecessary

--- static/js/scroll.js
+++ static/js/scroll.js
@@ -4,14 +4,4 @@ $(document).ready(function() {
             scrollTop: $("#main").offset().top
         }, 1000);
     });
-    $("#scrollLinkMap").click(function() {
-        $('html, body').animate({
-            scrollTop: $("#map").offset().top
-        }, 1000);
-    });
-    $("#scrollLinkForm").click(function() {
-        $('html, body').animate({
-            scrollTop: $("#addressForm").offset().top
-        }, 1000);
-    });
 });
