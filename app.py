c81d51c698617d89ee57f56b0f501fc545dbcd55
Natalija Bujevic <natalijabujevic0708@gmail.com>
2020-09-25 12:12:05 +0100

    Check if the input address is valid
    Check if it's an acctual address, if it's in Cork City and that it doesn't already exist in the database

--- app.py
+++ app.py
@@ -84,8 +84,7 @@ def edit_location(location_id):
 
 # to update a location
 
-
-@app.route('/update_location/<location_id>', methods=["POST"])
+@app.route('/update_location/<location_id>', methods=['POST'])
 def update_location(location_id):
     location = mongo.db.active_locations.find_one(
         {"_id": ObjectId(location_id)})
@@ -105,7 +104,6 @@ def update_location(location_id):
 
 # delete_location.html
 
-
 @app.route('/delete_location/<location_id>')
 def delete_location(location_id):
     if 'username' in session:
@@ -116,8 +114,7 @@ def delete_location(location_id):
 
 # to delete a location
 
-
-@app.route('/delete_location_update/<location_id>', methods=["POST"])
+@app.route('/delete_location_update/<location_id>', methods=['POST'])
 def delete_location_update(location_id):
 <<<<<<< HEAD
     locations = mongo.db.locations
@@ -154,7 +151,6 @@ def delete_location_update(location_id):
 
 # cleaned_locations.html
 
-
 @app.route('/cleaned_locations')
 def cleaned_locations():
     return render_template("cleaned_locations.html", locations=mongo.db.active_locations.find())
@@ -168,8 +164,12 @@ def cleaned_location_details(location_id):
     Find the current location by the location_id, and set the necessary variables needed for the template (src, src_cleaned, address, name, date and
     number of people).
     """
+<<<<<<< HEAD
     
 >>>>>>> d95e7c1... Add key:value pair number_of_people
+=======
+
+>>>>>>> 2937988... Check if the input address is valid
     the_location_details = mongo.db.active_locations.find_one(
         {"_id": ObjectId(location_id)})
     address = the_location_details['address_of_location']
@@ -183,11 +183,16 @@ def cleaned_location_details(location_id):
 =======
     date = the_location_details['date_of_cleanup']
     number_of_people = the_location_details['number_of_people']
+<<<<<<< HEAD
     
     return render_template('cleaned_location_details.html', src=src, src_cleaned=src_cleaned, address=address, name=name, date=date, number_of_people = number_of_people)
 >>>>>>> d95e7c1... Add key:value pair number_of_people
 
 # register.html
+=======
+
+    return render_template('cleaned_location_details.html', src=src, src_cleaned=src_cleaned, address=address, name=name, date=date, number_of_people=number_of_people)
+>>>>>>> 2937988... Check if the input address is valid
 
 
 @app.route('/register', methods=['POST', 'GET'])
@@ -235,7 +240,6 @@ def login():
 
 # profile.html
 
-
 @app.route('/profile_page')
 def profile_page():
     if 'username' in session:
@@ -246,6 +250,7 @@ def profile_page():
 @app.route('/profile_edit_location/<location_id>')
 def profile_edit_location(location_id):
     the_location_details = mongo.db.active_locations.find_one(
+<<<<<<< HEAD
         {"_id": ObjectId(location_id)})
     src = url_for('picture', picture_name=the_location_details['picture_name'])
     return render_template('profile_edit_location.html', location=the_location_details,  src=src)
@@ -258,8 +263,17 @@ def profile_edit_locationandpicture(location_id):
 
 
 @app.route('/profile_update_location/<location_id>', methods=["POST"])
+=======
+        {'_id': ObjectId(location_id)})
+
+    return render_template('profile_edit_location.html', location=the_location_details)
+
+
+@app.route('/profile_update_location/<location_id>', methods=['POST', 'GET'])
+>>>>>>> 2937988... Check if the input address is valid
 def profile_update_location(location_id):
     location = mongo.db.active_locations.find_one(
+<<<<<<< HEAD
         {"_id": ObjectId(location_id)})
     locations = mongo.db.active_locations
     locations.update({'_id': ObjectId(location_id)},
@@ -276,6 +290,47 @@ def profile_update_location(location_id):
 
 @app.route('/profile_update_locationandpicture/<location_id>', methods=["POST"])
 def profile_update_locationandpicture(location_id):
+=======
+        {'_id': ObjectId(location_id)})
+
+    # using geolocator to determine latitude and longitude
+    locations = mongo.db.active_locations
+    geolocator = Nominatim(user_agent='http://127.0.0.1:5000/locations')
+    address = request.form.get('address')
+    loc = geolocator.geocode(address)
+
+    # make sure the user entered a valid address
+    if loc: 
+        # make sure the user entered an address in Cork City
+        if 51.86 <= loc.latitude <= 51.92 and -8.54 <= loc.longitude <= -8.41:
+            locations.update({'_id': ObjectId(location_id)},
+                                {'status': location['status'],
+                                'address_of_location': request.form.get('address'),
+                                'picture_name': location['picture_name'],
+                                'uploaded_by':  session['username'],
+                                'date': time.strftime("%Y-%m-%d %H:%M:%S"),
+                                'latitude_of_location': loc.latitude,
+                                'longitude_of_location': loc.longitude
+                                })
+            src = url_for('picture', picture_name=location['picture_name'])
+            return render_template('profile_edit_picture.html', location=location, location_id=location_id, src=src)
+
+        flash('Address you have entered is outside of Cork City')
+        return render_template('profile_edit_location.html',  location_id=location_id, location=location)
+
+    flash('Invalid address')
+    return render_template('profile_edit_location.html',  location_id=location_id, location=location)
+    
+
+
+@app.route('/profile_update_picture/<location_id>', methods=['POST'])
+def profile_update_picture(location_id):
+    """
+    Find the current location by the location_id, update the location with the new information from the form.
+    The information not gathered in the form remain the same. 
+    """
+
+>>>>>>> 2937988... Check if the input address is valid
     location = mongo.db.active_locations.find_one(
         {"_id": ObjectId(location_id)})
     locations = mongo.db.active_locations
@@ -284,7 +339,7 @@ def profile_update_locationandpicture(location_id):
     locations = mongo.db.active_locations
     locations.update({'_id': ObjectId(location_id)},
                      {'status': location['status'],
-                      'address_of_location': request.form.get('address'),
+                      'address_of_location': location['address_of_location'],
                       'picture_name':  picture.filename,
                       'uploaded_by':  session['username'],
                       'date': time.strftime("%Y-%m-%d %H:%M:%S"),
@@ -295,6 +350,7 @@ def profile_update_locationandpicture(location_id):
     return render_template('profile.html', locations=mongo.db.active_locations.find())
 
 
+<<<<<<< HEAD
 # to insert a location on locations.html
 
 
@@ -321,6 +377,68 @@ def insert_location():
             return redirect(url_for('locations'))
     flash("Please log in to add a location")
     return redirect(url_for('locations'))
+=======
+@app.route('/insert_location', methods=['POST'])
+def insert_location():
+    """
+    When a user inputs an address:
+
+        1. Check if the address is valid.
+
+        2. Check the address is in Cork City.
+
+        3. Check if the address is already in the database.
+
+        4. Insert the data to the database.
+
+    """
+    if 'picture' in request.files:
+        picture = request.files['picture']
+        mongo.save_file(picture.filename, picture)
+        # use geolocator to determine latitude and longitude
+        geolocator = Nominatim(user_agent='http://127.0.0.1:5000/locations')
+        address = request.form.get('address')
+        loc = geolocator.geocode(address)
+        # make sure the user entered a valid address
+        if loc: 
+            # make sure the user entered an address in Cork City
+            if 51.86 <= loc.latitude <= 51.92 and -8.54 <= loc.longitude <= -8.41:
+                # make sure the database is not empty
+                if mongo.db.active_locations.find().count() > 0:
+                    # make sure the adress is not laready in the database
+                    for i in mongo.db.active_locations.find():
+                        if i['latitude_of_location'] != loc.latitude or i['longitude_of_location'] != loc.longitude:
+                            mongo.db.active_locations.insert({
+                                'status': 'not_cleaned',
+                                'address_of_location': address,
+                                'picture_name': picture.filename,
+                                'uploaded_by': session['username'],
+                                'date': time.strftime("%Y-%m-%d %H:%M:%S"),
+                                'latitude_of_location': loc.latitude,
+                                'longitude_of_location': loc.longitude
+                            })
+                            return redirect(url_for('locations'))
+                        
+                        flash('This address already exists')
+                        return redirect(url_for('locations'))
+
+                mongo.db.active_locations.insert({
+                                'status': 'not_cleaned',
+                                'address_of_location': address,
+                                'picture_name': picture.filename,
+                                'uploaded_by': session['username'],
+                                'date': time.strftime("%Y-%m-%d %H:%M:%S"),
+                                'latitude_of_location': loc.latitude,
+                                'longitude_of_location': loc.longitude
+                })
+                return redirect(url_for('locations'))
+
+            flash('Address you have entered is outside of Cork City')
+            return redirect(url_for('locations'))
+
+        flash('Invalid address')
+        return redirect(url_for('locations'))
+>>>>>>> 2937988... Check if the input address is valid
 
 <<<<<<< HEAD
 <<<<<<< HEAD
