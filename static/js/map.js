d2e6cbf43ec44e2317d53ff98e98095a1bc359b8
Natalija Bujevic <natalijabujevic0708@gmail.com>
2020-09-26 18:32:40 +0100

    Make sure only one infowindow is open at all times

--- static/js/map.js
+++ static/js/map.js
@@ -1,14 +1,5 @@
-// info window when clicked on the marker
-function addInfoWindow(marker, message) {
-    var infoWindow = new google.maps.InfoWindow({
-        content: message
-    });
-    google.maps.event.addListener(marker, 'click', function () {
-        infoWindow.open(map, marker);
-    });
-}
 
-// creating the map
+// Create the map
 function initMap() {
     var map = new google.maps.Map(document.getElementById("map"), {
         zoom: 12,
@@ -17,30 +8,46 @@ function initMap() {
             lng: -8.468399
         }
     });
-    //creating markers
+
+    // Create the markers
     var items_locations = document.querySelectorAll(".address_of_locations");
     for (var i = 0; i < items_locations.length; i++) {
         var marker = new google.maps.Marker({
             position: { lat: parseFloat(this.document.getElementsByClassName('lat')[i].innerHTML), lng: parseFloat(this.document.getElementsByClassName('long')[i].innerHTML) },
             map: map,
         });
-        addInfoWindow(marker, this.document.getElementsByClassName('address')[i].innerHTML);
+        infoWindow(marker, this.document.getElementsByClassName('address')[i].innerHTML);
     };
-        // default bounds for autocomplete
-        var defaultBounds = new google.maps.LatLngBounds(
-            new google.maps.LatLng(51.8722517, -8.53925228),
-            new google.maps.LatLng(51.91886165, -8.42110634))
-        var options = {
-            bounds: defaultBounds,
-            strictBounds: true,
-            types: ['address']
-        };
-        var input = document.getElementById('input_address');
 
-        autocomplete = new google.maps.places.Autocomplete(input, options)
-        
+    // Create info window when clicked on the marker
+    var iw = new google.maps.InfoWindow()
+    function infoWindow(marker, message) {
+        google.maps.event.addListener(marker, 'click', function () {
+            var html = "<p id='search_address' onclick = search_address()>" + message + "</p>"
+            // set the content 
+            iw.setContent(html);
+            // open the infowindow on the marker
+            iw.open(map, marker);
+        });
+    };
+
+    // Create default bounds for autocomplete
+    var defaultBounds = new google.maps.LatLngBounds(
+        new google.maps.LatLng(51.8722517, -8.53925228),
+        new google.maps.LatLng(51.91886165, -8.42110634))
+    var options = {
+        bounds: defaultBounds,
+        strictBounds: true,
+        types: ['address']
+    };
+    var input = document.getElementById('input_address');
+
+    autocomplete = new google.maps.places.Autocomplete(input, options)
+
 
 }
+
+
 
 
 
