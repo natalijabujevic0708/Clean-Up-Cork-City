fb8b21d0fc56ab8be618e4a83f7d2b4595676858
Natalija Bujevic <natalijabujevic0708@gmail.com>
2020-09-27 23:21:21 +0100

    Redirect user to address form when clicking "Add a new location"
    Set the value of scrollPosition_/locations to 1800 when a user clicks the add link

--- static/js/retain_scroll_position.js
+++ static/js/retain_scroll_position.js
@@ -5,8 +5,12 @@ $(function () {
     window.onbeforeunload = function () {
         var scrollPosition = $(document).scrollTop();
         sessionStorage.setItem("scrollPosition_" + pathName, scrollPosition.toString());
+        
     }
     if (sessionStorage["scrollPosition_" + pathName]) {
         $(document).scrollTop(sessionStorage.getItem("scrollPosition_" + pathName));
     }
 });
+function go_to_form(){
+    sessionStorage.setItem("scrollPosition_/locations", "1800"); // to go back to the form
+}
