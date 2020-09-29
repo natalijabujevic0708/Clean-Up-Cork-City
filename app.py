609c385173e3211b4f96570c2acea5049c0d95cb
Natalija Bujevic <natalijabujevic0708@gmail.com>
2020-09-29 22:57:34 +0100

    Add os before environ to set the enviroment variables

--- app.py
+++ app.py
@@ -8,8 +8,8 @@ from geopy.geocoders import Nominatim
 
 app = Flask(__name__)
 
-app.config['MONGO_DBNAME'] =  environ.get('MONGO_DBNAME')
-app.config['MONGO_URI'] = environ.get('MONGO_URI')
+app.config['MONGO_DBNAME'] =  os.environ.get('MONGO_DBNAME')
+app.config['MONGO_URI'] = os.environ.get('MONGO_URI')
 
 mongo = PyMongo(app)
 
