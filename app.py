12d966bb99aabadbb639f8e7504ae9a657ed9775
Natalija Bujevic <natalijabujevic0708@gmail.com>
2020-10-06 13:15:33 +0100

    Set debug=False

--- app.py
+++ app.py
@@ -592,4 +592,4 @@ def page_not_found(error):
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT', '5000')),
-            debug=True)
+            debug=False)
