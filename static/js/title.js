3b5d8f7cd852b035183b35a4a4e1ee65a7ab0b2a
Natalija Bujevic <natalijabujevic0708@gmail.com>
2020-10-06 11:44:44 +0100

    Correct the structure of the  profile.html
    Place the section element inside the for loop so the title.js wouls work correctly

--- static/js/title.js
+++ static/js/title.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
     if ($('#uploaded_locations').length) {
         $("#title").html("The locations you uploaded:");
-    } else {
+    }else {
         $("#title").html("Upload your first location<br>or<br> join us for a cleanup event!");
     };
 });
